4abd01a9374baac98d4293084dd77cf94cb149f6
Domingo Rodriguez <domingorodriguez2012@gmail.com>
2021-03-02 16:23:47 -0600

    update

--- java-testing-framework/src/main/java/com/enterprise/model/Stopwatch.java
+++ java-testing-framework/src/main/java/com/enterprise/model/Stopwatch.java
@@ -6,9 +6,26 @@ public class Stopwatch {
     LocalTime startedTICK;
     LocalTime endedTOCK;
     LocalTime elapsedTICKTOCK;
-    public Stopwatch(){
-        Stopwatch mytimer = new Stopwatch();
-        mytimer.startedTICK = LocalTime.now();
 
+    Stopwatch myTimer = new Stopwatch();
+
+    public Stopwatch(){
+        startStopwatch();
+    }
+
+
+    public void startStopwatch()
+    {
+        this.startedTICK = LocalTime.now();
     };
+
+    public void stopStopWatch()
+    {
+        this.endedTOCK = LocalTime.now();
+    }
+
+    public int getElapsedTime()
+    {
+        return this.endedTOCK.minus(this.startedTICK, );
+    }
 }
