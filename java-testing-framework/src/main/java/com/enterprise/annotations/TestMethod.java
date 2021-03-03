47d8f8096c4087dc1bb02bb69544ed4fe4fae60b
John Howland Stone <john.howland.stone@gmail.com>
2021-03-03 15:53:05 -0700

    Adds todo

--- java-testing-framework/src/main/java/com/enterprise/annotations/TestMethod.java
+++ java-testing-framework/src/main/java/com/enterprise/annotations/TestMethod.java
@@ -5,6 +5,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @Documented
+//TODO: Change Annotation to take generics
 public @interface TestMethod {
 
     String name() default "standard test";
