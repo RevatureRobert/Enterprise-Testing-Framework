97bb93c721eff6e06d385e3e3c0280dd51675228
Kevin <kwang44@buffalo.edu>
2021-03-04 10:07:57 -0500

    fixing hashmap

--- java-testing-framework/src/main/java/com/enterprise/util/HashMap.java
+++ java-testing-framework/src/main/java/com/enterprise/util/HashMap.java
@@ -1,54 +1,130 @@
 package com.enterprise.util;
 
+
 public class HashMap<K,V> {
 
-    Node<K, V> buckets[];
-    int size;
-    int capacity;
-    double loadFactor;
+    private Node<K, V> buckets[];
+    private int size;
+    private int capacity;
+    private int loadFactor;
 
     public HashMap() {
         capacity = 10;
         buckets = new Node[capacity];
         size = 0;
-        loadFactor = .5;
+        loadFactor = 2;
     }
 
-    public boolean add(K key, V value) {
-
-        //refactor when loadFactor is exceeded.
-        if (size / capacity > loadFactor) {
-            capacity *= 2;
+    public V put(K key, V value) {
+        if (Math.abs(capacity / (size + 1)) < loadFactor) {
+            capacity = capacity * 2;
             Node<K, V> newBuckets[] = new Node[capacity];
+
+            for (int i = 0; i < buckets.length; i++) {
+                if (buckets[i] == null)
+                    continue;
+
+                Node<K, V> currentNode = buckets[i];
+
+                while (currentNode != null) {
+                    int index = getIndex(currentNode.key);
+                    if (newBuckets[index] == null) {
+                        newBuckets[index] = currentNode;
+                        currentNode = currentNode.next;
+                        newBuckets[index].next = null;
+                    }
+                    else {
+                        Node<K, V> placeholder = currentNode;
+                        currentNode = currentNode.next;
+                        placeholder.next = newBuckets[index];
+                        newBuckets[index] = placeholder;
+                    }
+                }
+            }
+            buckets = newBuckets;
         }
 
-        return false;
+        int index = getIndex(key);
+
+        if (buckets[index] == null) {
+            buckets[index] = new Node<K, V>(key, value);
+            return value;
+        }
+
+        Node<K, V> currentNode = buckets[index];
+
+        while (currentNode.next != null) {
+            if (currentNode.key.equals(key))
+                return null;
+            currentNode = currentNode.next;
+        }
+
+        if (currentNode.key.equals(key))
+            return null;
+
+        currentNode.next = new Node<K, V>(key, value);
+        size++;
+        return value;
     }
 
     public V remove(K key) {
+        int index = getIndex(key);
+        if (buckets[index] == null)
+            return null;
+
+        Node<K, V> currentNode = buckets[index];
+
+        if (currentNode.key.equals(key)) {
+            buckets[index] = null;
+            size--;
+            return currentNode.value;
+        }
+
+        Node<K, V> previousNode = currentNode;
+        currentNode = currentNode.next;
+
+        while (currentNode != null) {
+            if (currentNode.key.equals(key)) {
+                previousNode.next = currentNode.next;
+                size--;
+                return currentNode.value;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
         return null;
     }
 
     public V get(K key) {
+        int index = getIndex(key);
+        Node<K, V> currentNode = buckets[index];
+
+        while (currentNode != null) {
+            if (currentNode.key.equals(key))
+                return currentNode.value;
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
-}
+    public int getSize() { return size; }
 
-class Node<K, V> {
-    K key;
-    V data;
-    Node next;
 
-    public Node(K key, V data) {
-        this.key = key;
-        this.data = data;
-        this.next = null;
+
+    private int getIndex(K key) {
+        return Math.abs(key.hashCode() % capacity);
     }
 
-    public Node(K key, V data, Node next) {
-        this.key = key;
-        this.data = data;
-        this.next = next;
+    private class Node<K, V> {
+        K key;
+        V value;
+        Node next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
     }
 }
